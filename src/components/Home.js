257d5ca146c1ea9516f4b4c6c5250e6d8a466bb0
Bouyahi Zakaria <bouyahiazakaria@gmail.com>
2022-06-24 08:48:28 +0100

    Api checkpoint

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Home Page!</h1>
+            <h2 style={{ textAlign: "left" }}>Qu'est-ce que le Lorem Ipsum?</h2>
+            <p style={{textDecoration:"justify"}}>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment,
+                par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+        </div>
+    )
+}
+
+export default Home
